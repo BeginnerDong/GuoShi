13099b328d7795ca5f2275b6af66a5fad84090be
Mrzhzhzh <40493350+Mrzhzhzh@users.noreply.github.com>
2020-07-03 18:36:48 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,8 +1,7 @@
 window.base = {
-	g_restUrl: 'http://106.12.155.217/goldenfox/public/index.php/api/v1/',
+	g_restUrl: 'http://106.12.155.217/guoshi/public/index.php/api/v1/',
 
 	thirdapp_id:2,
-	test:666,
 	bus:new Vue(),
 	getUserToken: function(callback) {
 
